865b9ef23f425afe6fb1d6af3ef4c248e342edb7
Abdul Hadi Khan <ahkn63@gmail.com>
2026-05-27 13:49:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (64.91%)

---
+++ LeetCode-Problems/0392-is-subsequence/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s: return True
+        sCompareIndex  = 0
+        for tLetter in t:
+            if tLetter == s[sCompareIndex]:
+                sCompareIndex  += 1
+                if sCompareIndex  == len(s): return True      
+        return False
